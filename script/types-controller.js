ff82063112cc2f2db674d37d3a97fb1fa86e9eb5
jcbarbaf <jcbarbaf@gmail.com>
2025-12-12 12:36:37 +0100

    Types covered finished

--- script/types-controller.js
+++ script/types-controller.js
@@ -1,15 +1,20 @@
 import { typeRelations } from "./type-relations.js"
 
-let typesOwned = [1, 11]
+let typesOwned = []
 document.querySelector('.selector').addEventListener('click', (event) => {
     let type = event.target.closest('.type')
     if (type) {
         type.classList.toggle('active')
-        alert(type.dataset.typeID)
+        if (type.classList.contains('active')) {
+            typesOwned.push(type.dataset.typeId)
+        } else {
+            let index = typesOwned.indexOf(type.dataset.typeId)
+            typesOwned.splice(index, 1)
+        }
+        calculateTypesCovered()
     }
 })
 
-calculateTypesCovered()
 
 function calculateTypesCovered () {
     let typesCovered = []
@@ -22,7 +27,11 @@ function calculateTypesCovered () {
 }
 
 function showTypesCovered (typesCovered) {
+    document.querySelectorAll('.covered .type').forEach(typeElement => {
+        typeElement.classList.remove('active')
+    })
     typesCovered.forEach(type => {
         console.log(type)
+        document.querySelector(`.covered .${type}`).classList.add('active')
     })
 }
